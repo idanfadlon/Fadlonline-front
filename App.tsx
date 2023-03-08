cd0b0d2e46b6cba3853ad58a805f5a5c50bec3d0
idanfadlon <66566962+idanfadlon@users.noreply.github.com>
2023-03-08 16:56:49 +0200

    add all components that i need to implemente

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Splash from './components/splash';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-export default function App() {
+const App = () => {
+  useEffect(() => {
+    async function hideSplashScreen() {
+      await SplashScreen.preventAutoHideAsync();
+      // Simulate a heavy load task
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      await SplashScreen.hideAsync();
+    }
+    hideSplashScreen();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Splash/>
     </View>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ components/splash.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Image, View } from "react-native"
+
+
+const Splash = ()=>{
+    return(
+        <View>
+          
+          <Image source={require('.assets/fadlonlineSplash.png') }/>
+
+        </View>
+    )
+}
+export default Splash
